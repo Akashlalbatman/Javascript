b1be670fe7acaba6d60fa6b6482bb592e6c48771
Akashlalbatman <143404233+Akashlalbatman@users.noreply.github.com>
2023-10-24 16:07:54 +0530

    Add files via upload

---
+++ Assign1.js
@@ -0,0 +1,4 @@
+document.getElementById("p1").innerHTML="Hello Javascript";
+document.getElementById('p1').style.color="red";
+document.getElementById('intro').style.fontsize="45px";
+document.body.style.backgroundColor="lightyellow";
---
+++ Assign2.js
@@ -0,0 +1,9 @@
+document.body.style.backgroundColor="lightyellow";
+document.getElementById("heading").innerHTML="Javascript";
+document.getElementById('heading').style.fontsize="35px";
+document.getElementById("p1").innerHTML="Javascript is a powerful,flexible and fast programming language.";
+document.getElementById("p2").innerHTML="Javascript powers the dynamic behaviour on websites.";
+document.getElementById("p3").innerHTML="Javascript remains at the core of web development.";
+document.getElementById('p1').style.color="red";
+document.getElementById('p2').style.color="blue";
+document.getElementById('p3').style.color="green";
